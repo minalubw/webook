75ee4f0c974dd445a7eca5e39db4f5a6fecd826e
minalubw <minalubiresaw@gmail.com>
2023-04-21 12:37:00 -0500

    auth

--- weebook-backend/models/usersModel.js
+++ weebook-backend/models/usersModel.js
@@ -6,4 +6,17 @@ const userSchema = new Schema({
     password: {type: String, required: true}
 }, {timestamps: true});
 
+userSchema.pre('save', async function (next) {
+    const user = this;
+  
+    if (!user.isModified('password')) {
+      return next();
+    }
+  
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+  
+    next();
+  });
 export default model('User', userSchema);
