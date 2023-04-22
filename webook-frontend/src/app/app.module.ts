71dfec21ef2cd3fb0eeee734accbadf8859e698f
minalubw <minalubiresaw@gmail.com>
2023-04-22 17:51:24 -0500

    changes

--- webook-frontend/src/app/app.module.ts
+++ webook-frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { SignOutComponent } from './sign-out/sign-out.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { UserBookingsComponent } from './user-bookings/user-bookings.component';
 import { RoomsComponent } from './rooms/rooms.component';
@@ -14,7 +13,6 @@ import { BookRoomComponent } from './book-room/book-room.component';
   declarations: [
     AppComponent,
     SignInComponent,
-    SignOutComponent,
     UserHomeComponent,
     UserBookingsComponent,
     RoomsComponent,
